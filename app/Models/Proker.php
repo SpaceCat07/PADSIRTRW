9af3ba3223a33a63925a184d0e5744ace0439b29
Ahmad Khan <nuzulakhanahmadfirdausi@gmail.com>
2024-11-25 14:55:19 +0700

    feat : Add Proker by Admin_RT or Admin_RW

--- app/Models/Proker.php
+++ app/Models/Proker.php
@@ -14,8 +14,22 @@ class Proker extends Model
     protected $fillable = [
         'judul',
         'isi',
-        'tanggal_mulai',
-        'tanggal_selesai',
-        'status'
+        'waktu',
+        'tanggal_pelaksanaan',
+        'status',
+        'lokasi',
+        'gambar'
     ];
+
+    // Relasi ke model DetailProkerRT
+    public function detailProkerRT()
+    {
+        return $this->hasMany(DetailProkerRT::class, 'id_proker'); // id_proker adalah foreign key di detail_proker_rt
+    }
+
+    // Relasi ke model DetailProkerRW
+    public function detailProkerRW()
+    {
+        return $this->hasMany(DetailProkerRW::class, 'id_proker'); // id_proker adalah foreign key di detail_proker_rw
+    }
 }
