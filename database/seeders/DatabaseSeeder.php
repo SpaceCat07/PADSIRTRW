fd15f93853e84d6d5badeefeded84e09914cf216
Ahmad Khan <nuzulakhanahmadfirdausi@gmail.com>
2024-12-03 22:13:02 +0700

    feat : pembayaran iuran rw oleh admin rt, manage to [selesai, gagal]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RTSeeder::class,
             WargaSeeder::class,
             IuranRTSeeder::class,
+            IuranRWSeeder::class,
         ]);
 
     }
