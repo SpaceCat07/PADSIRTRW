438e63952da44f9aea70a107dcd625563cef7dec
Ahmad Khan <nuzulakhanahmadfirdausi@gmail.com>
2025-01-19 16:15:42 +0700

    fix : user can only critize its own RT, cannot critisize RW, there are no name input

--- resources/views/kritiksaran/index.blade.php
+++ resources/views/kritiksaran/index.blade.php
@@ -5,22 +5,7 @@
 @endif
 <form action="{{route('kritik.store')}}" method="post">
     @csrf
-
-    <select name="tujuan" id="">
-        @foreach ($listRW as $rw)
-            <option value="{{ $rw -> nama_rw }}">{{ $rw -> nama_rw }}</option>
-        @endforeach
-        @foreach ($listRT as $rt)
-            <option value="{{ $rt -> nama_rt }}">{{ $rt -> nama_rt}}</option>
-        @endforeach
-    </select>
-    @error('role')
-    <div class="alert alert-danger mt-2">
-        {{$message}}
-    </div>
-    @enderror
-
-    <input type="text" name="isi" id="" placeholder="Isi Kritik/Saran">
+    <input type="text" name="isi" id="" placeholder="Isi Kritik/Saran" style="height: 200px; border: 1px solid white;">
     @error('isi')
         <div class="alert alert-danger mt-2">
             {{$message}}
