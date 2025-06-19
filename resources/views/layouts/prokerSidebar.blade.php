c8f9ebd19e67681184aac2ad94044843c5218cb7
krisvananta <afifkrisvananta@gmail.com>
2025-06-19 08:12:02 +0700

    feat: fix bug dropdown tidak bisa di klik dari laman program kerja (warga)

--- resources/views/layouts/prokerSidebar.blade.php
+++ resources/views/layouts/prokerSidebar.blade.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap and CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
 
